6509b5e45e83333d0742b9f704d46260654f0a1e
Alina <adrizhinskaya@bk.ru>
2025-05-25 14:55:47 +0300

    feat: create user-service

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    private String email;
+
+    private Integer age;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id serial primary key,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
