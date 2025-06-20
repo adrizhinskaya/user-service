3123faf9c9f94db2d8ce839ce495bddda3604293
Alina <adrizhinskaya@bk.ru>
2025-06-20 17:04:55 +0300

    feat: add swagger

--- src/main/java/org/example/model/dto/UserDto.java
+++ src/main/java/org/example/model/dto/UserDto.java
@@ -1,7 +1,9 @@
 package org.example.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 
@@ -10,11 +12,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(of = {"id"})
-public class UserDto {
+@Schema(description = "Пользователь")
+public class UserDto extends RepresentationModel<UserDto> {
+    @Schema(description = "Идентификатор", example = "6", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+    @Schema(description = "Имя", maxLength = 250, minLength = 2, example = "Иван Петров")
     private String name;
+    @Schema(description = "Почтовый адрес", maxLength = 254, minLength = 6, example = "ivan.petrov@mail.ru")
     private String email;
+    @Schema(description = "Возраст", example = "18")
     private Integer age;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Schema(description = "Дата и время создания пользователя. " +
+            "Дата и время указываются в формате \"yyyy-MM-dd HH:mm:ss\"", example = "2023-10-11 23:10:05")
     private LocalDateTime createdAt;
+
 }
